e0fc2e0d6ccdab26930c1a0f0b96fd2942da4920
Lukas <zitronen.doris@yahoo.de>
2015-06-02 20:08:33 +0200

    test

---
+++ sportverein/src/main/java/webprojekt/sportverein/App.java
@@ -0,0 +1,13 @@
+package webprojekt.sportverein;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
